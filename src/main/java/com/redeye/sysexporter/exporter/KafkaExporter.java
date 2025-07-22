fbdd87f6af3a3d1800ac523c2f0ce2dbf4a0d67c
jung mo <gromit25@hanmail.net>
2025-07-22 09:52:05 +0900

    Update KafkaExporter.java

--- src/main/java/com/redeye/sysexporter/exporter/KafkaExporter.java
+++ src/main/java/com/redeye/sysexporter/exporter/KafkaExporter.java
@@ -1,21 +1,54 @@
 package com.redeye.sysexporter.exporter;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import com.jutools.StringUtil;
+
 /**
- * 
+ * Kafka Exporter
  * 
  * @author jmsohn
  */
 public class KafkaExporter extends Exporter {
 
-	/** */
+	/** kafka 전송 객체 */
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
-
+	
+	/** kafka 전송 토픽 */
+	@Value("${app.exporter.kafka.topic}")
+	private String topic;
+	
+	
 	@Override
 	public void send(String message) throws Exception {
-		this.kafkaTemplate.send("", message);
+		
+		if(StringUtil.isBlank(message) == true) {
+			return;
+		}
+		
+		this.kafkaTemplate.send(this.topic, getKey(message), message);
+	}
+	
+	/**
+	 * kafka 전송시 사용할 key를 생성하여 반환함
+	 * 
+	 * @param message 전송할 메시지
+	 * @return 생성된 key
+	 */
+	private static String getKey(String message) {
+		
+		JSONObject messageJSON = new JSONObject(message);
+
+		// sys-exporter:host:type
+		return new StringBuilder()
+			.append("sys-exporter:")
+			.append(messageJSON.getString("host"))
+			.append(":")
+			.append(messageJSON.getString("type"))
+			.toString();
 	}
 }

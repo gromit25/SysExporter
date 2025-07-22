47c1743c3fcbf3ddc2621be8a82bbc45a69e5c95
jung mo <gromit25@hanmail.net>
2025-07-22 10:02:11 +0900

    Update PrintExporter.java

--- src/main/java/com/redeye/sysexporter/exporter/PrintExporter.java
+++ src/main/java/com/redeye/sysexporter/exporter/PrintExporter.java
@@ -1,7 +1,5 @@
 package com.redeye.sysexporter.exporter;
 
-import org.springframework.stereotype.Component;
-
 /**
  * printer exporter 컴포넌트
  *
@@ -11,6 +9,6 @@ public class PrintExporter extends Exporter {
 
 	@Override
 	public void send(String message) throws Exception {
-		System.out.println(message);
+		System.out.println("RECV: " + message);
 	}
 }

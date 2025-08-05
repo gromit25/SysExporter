d88c0babe324341effed4fab4130c570ceda2675
jung mo <gromit25@hanmail.net>
2025-08-05 14:19:37 +0900

    Update RestAPIExporter.java

--- src/main/java/com/redeye/sysexporter/exporter/restapi/RestAPIExporter.java
+++ src/main/java/com/redeye/sysexporter/exporter/restapi/RestAPIExporter.java
@@ -1,5 +1,10 @@
 package com.redeye.sysexporter.exporter.restapi;
 
+/**
+ *
+ *
+ * @author jmsohn
+ */
 @Component
 @ConditionalOnProperty
 (
@@ -8,12 +13,22 @@ package com.redeye.sysexporter.exporter.restapi;
 )
 public class RestAPIExporter extends Exporter {
 
-	private static final String SUBPATH_CPU = "";
-	private static final String SUBPATH_MEM = "";
-	private static final String SUBPATH_DISK = "";
-	private static final String SUBPATH_NET = "";
-	private static final String SUBPATH_PROCESS_TOP = "";
+	/** cpu 사용율 api subpath */
+	private static final String SUBPATH_CPU = "/host/%s/metrics/cpu";
+	
+	/** memory 사용율 api subpath */
+	private static final String SUBPATH_MEM = "/host/%s/metrics/memory";
+	
+	/** disk 사용율 api subpath */
+	private static final String SUBPATH_DISK = "/host/%s/metrics/disk";
+	
+	/** network io 사용율 api subpath */
+	private static final String SUBPATH_NETIO = "/host/%s/metrics/netio";
+	
+	/** process top 5 api subpath */
+	private static final String SUBPATH_PROCESS_TOP = "/host/%s/metrics/process/top";
 
+	/** rest api 연결 클라이언트 */
 	@Autowired
 	private WebClient client;
 

a890d4ced2127bd1c0a4ababb2716bc54ec68e47
jung mo <gromit25@hanmail.net>
2025-08-05 10:06:18 +0900

    Update ProcessMetricsAcquisitor.java

--- src/main/java/com/redeye/sysexporter/acquisitor/ProcessMetricsAcquisitor.java
+++ src/main/java/com/redeye/sysexporter/acquisitor/ProcessMetricsAcquisitor.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Component;
 
 import oshi.software.os.OSProcess;
 
+/**
+ * CPU 사용율 Top 5 프로세스의 성능 정보 수집
+ *
+ * @author jmsohn
+ */
 @Component
 public class ProcessMetricsAcquisitor extends Acquisitor {
 	
@@ -17,30 +22,38 @@ public class ProcessMetricsAcquisitor extends Acquisitor {
 
 	@Override
 	protected Map<String, Object> acquireMetrics() {
-/*		
-		List<OSProcess> procList = this.getSysInfo().getOperatingSystem().getProcesses();
+
+		// 시스템 정보 획득
+		OperatingSystem os = this.getSysInfo().getOperatingSystem();
+		CentralProcessor processor = this.getSysInfo().getHardware().getProcessor();
+
+		// 시스템 논리 코어 수
+		int logicalCoreCount = processor.getLogicalProcessorCount();
+	
+		// CPU 사용률 기준으로 상위 5개 프로세스 목록 획득
+		List<OSProcess> top5ProcesseList = os.getProcesses(null, ProcessSorting.CPU_DESC, 5);
+
+		// Top 5 프로세스 성능 정보 목록
+		List<Map<String, Object>> processMetricsList = new ArrayList<>();
 		
-		for(OSProcess proc: procList) {
+		for(int index = 0; index < top5ProcesseList.size(); index++) {
+
+			OSProcess process = top5ProcesseList.get(index);
 			
-			if(proc.getName().startsWith("java") == true) {
-			
-				StringBuilder procMetrics = new StringBuilder()
-					.append(proc.getName())
-					.append("\t")
-					.append(proc.getProcessCpuLoadCumulative())
-					.append("\t")
-					.append(proc.getResidentSetSize())
-					.append("byte");
-				
-				// argument 확인
-//				for(String arg: proc.getArguments()) {
-//					procMetrics.append(" ").append(arg);
-//				}
-				
-				System.out.println(procMetrics.toString());
-			}
+			// 프로세스 성능 정보 생성
+			Map<String, Object> processMetrics = new HashMap<>();
+
+			processMetrics.put("order", index);
+			processMetrics.put("pid", process.getProcessID());
+			processMetrics.put("name", process.getName());
+			processMetrics.put("cpuUsage", 100d * (process.getProcessCpuLoadCumulative() / logicalCoreCount));
+			processMetrics.put("memUsed", process.getResidentSetSize());
+			processMetrics.put("diskReadSize", process.getBytesRead());
+			processMetrics.put("diskWriteSize", process.getBytesWritten());
+
+			processMetricsList.add(processMetrics);
 		}
-*/
-		return null;
+
+		return processMetricsList;
 	}
 }

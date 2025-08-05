83fb043c7c50cce35b0bc22c80da0ceb015cf60c
jung mo <gromit25@hanmail.net>
2025-08-05 13:53:04 +0900

    Update RestAPIExporter.java

--- src/main/java/com/redeye/sysexporter/exporter/restapi/RestAPIExporter.java
+++ src/main/java/com/redeye/sysexporter/exporter/restapi/RestAPIExporter.java
@@ -3,15 +3,37 @@ package com.redeye.sysexporter.exporter.restapi;
 @Component
 @ConditionalOnProperty
 (
-  value = "app.exporter.type",
-  havingValue = "RESTAPI"
+	value = "app.exporter.type",
+	havingValue = "RESTAPI"
 )
 public class RestAPIExporter extends Exporter {
 
-  @Autowired
-  private WebClient client;
-  
-  @Override
+	private static final String SUBPATH_CPU = "";
+	private static final String SUBPATH_MEM = "";
+	private static final String SUBPATH_DISK = "";
+	private static final String SUBPATH_NET = "";
+	private static final String SUBPATH_PROCESS_TOP = "";
+
+	@Autowired
+	private WebClient client;
+
+	
+	@Override
 	public void send(String message) throws Exception {
-  }
+	}
+
+	private void sendCpuUsage(Map<String, Object> msgObj) throws Excepiton {
+	}
+
+	private void sendMem(Map<String, Object> msgObj) throws Exception {
+	}
+
+	private void sendDisk(Map<String, Object> msgObj) throws Exception {
+	}
+
+	private void sendNetIO(Map<String, Object> msgObj) throws Exception {
+	}
+
+	private void sendProcessTop(Map<String, Object> msgObj) throws Exception {
+	}
 }

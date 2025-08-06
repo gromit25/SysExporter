932dc3677ce43d087e54258b45922a461a22bb2a
jung mo <gromit25@hanmail.net>
2025-08-06 09:55:44 +0900

    Update RestAPIExporterConfig.java

--- src/main/java/com/redeye/sysexporter/exporter/restapi/RestAPIExporterConfig.java
+++ src/main/java/com/redeye/sysexporter/exporter/restapi/RestAPIExporterConfig.java
@@ -8,6 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
+/**
+ * Rest API 의 WebClient 생성
+ *
+ * @author jmsohn
+ */
 @Configuration
 @ConditionalOnProperty
 (
@@ -15,7 +20,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 	havingValue = "RESTAPI"
 )
 public class RestAPIExporterConfig {
-  
+	
+	/**
+	 * WebClient 객체 생성 및 반환
+	 *
+	 * @param url 기본 url
+  	 * @return 생성된 WebClient 객체
+	 */
 	@Bean("webClient")
 	public WebClient webClient(
 		@Value("app.exporter.restapi.url") String url

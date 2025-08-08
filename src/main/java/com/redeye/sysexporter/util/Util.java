c3d0de6375dfef613935549e381e1419d172409c
jung mo <gromit25@hanmail.net>
2025-08-08 12:56:47 +0900

    Update Util.java

--- src/main/java/com/redeye/sysexporter/util/Util.java
+++ src/main/java/com/redeye/sysexporter/util/Util.java
@@ -1,5 +1,7 @@
 package com.redeye.sysexporter.util;
 
+import org.json.JSONObject;
+
 import com.redeye.sysexporter.domain.HostVO;
 
 /**
@@ -9,22 +11,22 @@ import com.redeye.sysexporter.domain.HostVO;
  */
 public class Util {
 
-  /**
-   * 메시지 JSON 객체에서 호스트 정보 추출
-   *
-   * @param messageJSON 메시지 JSON 객체
-   * @return 호스트 정보
-   */
-  public static HostVO getHostInfo(JSONObject messageJSON) throws Exception {
+	/**
+	 * 메시지 JSON 객체에서 호스트 정보 추출
+	 *
+	 * @param messageJSON 메시지 JSON 객체
+	 * @return 호스트 정보
+	 */
+	public static HostVO getHostInfo(JSONObject messageJSON) throws Exception {
 
-    if(messageJSON == null) {
-      throw new IllegalArgumentException("messageJSON is null.");
-    }
+		if(messageJSON == null) {
+			throw new IllegalArgumentException("messageJSON is null.");
+		}
     
-    return new HostVO(
-      messageJSON.getString("organ"),
-      messageJSON.getString("region"),
-      messageJSON.getString("host")
-    );
-  }
+		return new HostVO(
+			messageJSON.getString("organ"),
+			messageJSON.getString("region"),
+			messageJSON.getString("host")
+		);
+	}
 }

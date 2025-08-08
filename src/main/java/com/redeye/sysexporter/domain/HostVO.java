2fb3656bcb03dfca19c46749ba505902f40b5825
jung mo <gromit25@hanmail.net>
2025-08-08 11:15:48 +0900

    Create HostVO.java

---
+++ src/main/java/com/redeye/sysexporter/domain/HostVO.java
@@ -0,0 +1,14 @@
+package com.redeye.sysexporter.domain;
+
+/**
+ * 호스트 정보 Value Object
+ *
+ * @param organ 조직명
+ * @param region 영역
+ * @param host 호스트명
+ */
+public record HostVO(
+  String organ,
+  String region,
+  String host
+);

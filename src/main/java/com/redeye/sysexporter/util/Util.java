ce0cb193a23ddf8ef72646c40a63cc77fe3164ef
jung mo <gromit25@hanmail.net>
2025-08-08 15:36:15 +0900

    Update Util.java

--- src/main/java/com/redeye/sysexporter/util/Util.java
+++ src/main/java/com/redeye/sysexporter/util/Util.java
@@ -12,21 +12,21 @@ import com.redeye.sysexporter.domain.HostVO;
 public class Util {
 
 	/**
-	 * 메시지 맵 객체에서 호스트 정보 추출
+	 * 메시지 JSON 객체에서 호스트 정보 추출
 	 *
-	 * @param msgMap 메시지 맵 객체
+	 * @param messageJSON 메시지 JSON 객체
 	 * @return 호스트 정보
 	 */
-	public static HostVO getHostInfo(Map<String, Object> msgMap) throws Exception {
+	public static HostVO getHostInfo(JSONObject messageJSON) throws Exception {
 
-		if(msgMap == null) {
-			throw new IllegalArgumentException("msgMap is null.");
+		if(messageJSON == null) {
+			throw new IllegalArgumentException("messageJSON is null.");
 		}
     
 		return new HostVO(
-			msgMap.getString("organ"),
-			msgMap.getString("region"),
-			msgMap.getString("host")
+			messageJSON.getString("organ"),
+			messageJSON.getString("region"),
+			messageJSON.getString("host")
 		);
 	}
 }

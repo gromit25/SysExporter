5347e6a49f4c3e82cb4aa398299be10780cd58ac
jung mo <gromit25@hanmail.net>
2025-08-08 11:48:56 +0900

    Update Util.java

--- src/main/java/com/redeye/sysexporter/util/Util.java
+++ src/main/java/com/redeye/sysexporter/util/Util.java
@@ -24,7 +24,7 @@ public class Util {
     return new HostVO(
       messageJSON.getString("organ"),
       messageJSON.getString("region"),
-      messageJSON.getString("host"),
+      messageJSON.getString("host")
     );
   }
 }

2fcff34410fdb5d0b4355433dd9104ebd6f9ad43
gromit25 <gromit25@hanmail.net>
2025-09-03 22:25:14 +0900

    버전 변경에 따른 수정

--- src/main/java/com/redeye/sysexporter/domain/HostTagVO.java
+++ src/main/java/com/redeye/sysexporter/domain/HostTagVO.java
@@ -1,6 +1,5 @@
 package com.redeye.sysexporter.domain;
 
-<<<<<<< HEAD
 import lombok.Data;
 
 /**
@@ -50,28 +49,3 @@ public class HostTagVO {
 			.toString();
 	}
 }
-=======
-/**
- * 호스트 테그 정보 Value Object
- *
- * @param organCode 조직 코드
- * @param domainCode 도메인 코드
- * @param host 호스트명
- */
-public record HostTagVO(
-  String organCode,
-  String domainCode,
-  String hostName
-) {
-  @Override
-  public String toString() {
-    return new StringBuilder()
-      .append(organCode())
-      .append(":")
-      .append(domainCode())
-      .append(":")
-      .append(hostName())
-      .toString();
-  }
-};
->>>>>>> branch 'main' of https://github.com/gromit25/SysExporter.git

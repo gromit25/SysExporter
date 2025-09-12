ef9be89aee825c61bc711af4fe1765a1aa68f856
jung mo <gromit25@hanmail.net>
2025-09-12 16:01:16 +0900

    Create Constants.java

---
+++ src/main/java/com/redeye/sysexporter/acquisitor/Constants.java
@@ -0,0 +1,11 @@
+package com.redeye.sysexporter.acquisitor;
+
+public class Constants {
+
+	public static final String CPU = "cpu";
+	public static final String MEM = "memory";
+	public static final String DISK = "disk";
+	public static final String NETWORK = "network";
+	public static final String TOP_PROCESS = "top-process";
+	public static final String TARGET_PROCESS = "target-process";
+}

3504f5cbecdf4540e6cd5dd0c55198ebcad04645
jung mo <gromit25@hanmail.net>
2025-09-12 16:14:23 +0900

    Update Constants.java

--- src/main/java/com/redeye/sysexporter/acquisitor/Constants.java
+++ src/main/java/com/redeye/sysexporter/acquisitor/Constants.java
@@ -3,7 +3,7 @@ package com.redeye.sysexporter.acquisitor;
 public class Constants {
 
 	public static final String CPU = "cpu";
-	public static final String MEM = "memory";
+	public static final String MEMORY = "memory";
 	public static final String DISK = "disk";
 	public static final String NETWORK = "network";
 	public static final String TOP_PROCESS = "top-process";
